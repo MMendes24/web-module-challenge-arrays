16f8dcf3728cc4646e3ae5fc9e0d04d6fcf12c3e
MMendes24 <mmbusiness1@protonmail.com>
2020-06-10 13:47:01 -0400

    Next series of tasks complete. Moving on to stretch goals. Browser issue fixed.

--- index.js
+++ index.js
@@ -40,18 +40,18 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(){
-    if (originalFlavors.length === 31){
-        console.log("There are 31 flavors!");
+function is31Flavors(flavors){
+    if (flavors.length === 31){
+        console.log("Woah! So many flavors!")
         return true;
     } else {
-        console.log("There are not 31 flavors? How can this be!");
+        console.log("Who took all of the flavors!")
         return false;
     }
 
 }
 
-is31Flavors()
+
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -64,13 +64,12 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(){
-originalFlavors.unshift("Rainbow Sherbert");
-console.log(originalFlavors);
+function addFlavor(flavor, flavorList){
+    flavorList.unshift("Rainbow Sherbert");
+    return console.log(flavorList);
 
 }
 
-addFlavor()
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
@@ -82,11 +81,12 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
+function removeLastFlavor(flavorList){
+    flavorList.pop()
+    return console.log(flavorList);
 
 }
+
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -97,11 +97,11 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
-
-    /*code here*/
+function getFlavorByIndex(flavorList, index){
+    return console.log(flavorList[index]);
 
 }
+
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -116,11 +116,13 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
-
-    /*code here*/
+function removeFlavorByName(flavorList,flavor){
+  
+    flavorList.splice(flavorList.indexOf(flavor), 1);
+    return console.log(originalFlavors);
 
 }
+
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -131,12 +133,12 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
-    /*code here*/
-
-}
-
+function copy(arr, newarr){
+    newarr = [...arr];
+    newarr.unshift("the sacred test!");
+    return newarr;
+    }
+    
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
